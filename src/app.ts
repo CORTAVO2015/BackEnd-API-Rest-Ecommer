6e5d15bec9d5a6c87b4660628550c36effa0594d
CORTAVO2015 <gustavoancortes@gmail.com>
2023-06-28 20:30:15 -0500

    [+] app.ts: Define 2 endpoints, /, /home

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,24 @@ import express from 'express';
 
 const app = express();
 
+/** Routing: Enrrutamiento */
+// http://localhost:3000/
+app.get('/', (req, res) => {
+    const message = 'Bienvenido a la API de Ecommerce';
+
+    console.log(message);
+    res.send(`<h1>${message}</h1>`);
+});
+
+
+// http://localhost:3000/home
+app.get('/home',(req, res) => {
+    const namePage = 'Home'
+
+    console.log(namePage);
+    res.send(`<h1>${namePage}</h1>`);
+});
+
 app.listen(3000, () => {
     console.log('Servidor lanzado en el puerto 3000');
 });
